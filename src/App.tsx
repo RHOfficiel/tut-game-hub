c434f8a278d53f75bf4cf11e739f492063e7010a
Rehder <trehderk@gmail.com>
2024-08-12 21:47:32 +0200

    Setup Done

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button, ButtonGroup } from "@chakra-ui/react";
+import LoginFinal from "./components/LoginFinal";
+
+function App() {
+  return (
+    <>
+      <LoginFinal />
+      <Button colorScheme="blue">Button</Button>
+    </>
+  );
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { CssVarsProvider } from "@mui/joy/styles";
+import CssBaseline from "@mui/joy/CssBaseline";
+import { ChakraProvider } from "@chakra-ui/react";
+import "@fontsource/inter";
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+const root = ReactDOM.createRoot(rootElement!);
+
+root.render(
+  <React.StrictMode>
+    <CssVarsProvider>
+      <CssBaseline />
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </CssVarsProvider>
+  </React.StrictMode>
+);
