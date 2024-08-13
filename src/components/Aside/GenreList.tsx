d96c5ca3f8031a63468faba500d9479ef08dc0a1
Rehder <trehderk@gmail.com>
2024-08-13 23:31:20 +0200

    Display Genres

--- src/components/Aside/GenreList.tsx
+++ src/components/Aside/GenreList.tsx
@@ -1,14 +1,52 @@
+import {
+  AspectRatio,
+  List,
+  ListItem,
+  ListItemDecorator,
+  Stack,
+  Typography,
+} from "@mui/joy";
 import useGenres from "../../hooks/useGenres";
+import getCroppedImageURL from "../../services/image-url";
 
 const GenreList = () => {
   const { data } = useGenres();
 
   return (
-    <ul>
+    <List>
       {data.map((genre) => (
-        <li key={genre.id}>{genre.name}</li>
+        <ListItem key={genre.id} sx={{ paddingY: 1 }}>
+          <ListItemDecorator sx={{ minWidth: 32, minHeight: 32 }}>
+            <AspectRatio
+              ratio="1"
+              sx={{
+                width: 48, // Ensure AspectRatio container is exactly 32x32
+                height: 48,
+                borderRadius: "20%", // Optional: to make it circular
+                overflow: "hidden", // Make sure the image is contained within
+                marginRight: 1, // Add some space between the image and text
+              }}
+            >
+              <img
+                src={getCroppedImageURL(genre.image_background)}
+                srcSet={
+                  getCroppedImageURL(genre.image_background) +
+                  "?auto=format&fit=crop"
+                }
+                loading="lazy"
+                alt={genre.name}
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  objectFit: "cover", // Ensures the image covers the container
+                }}
+              />
+            </AspectRatio>
+          </ListItemDecorator>
+          <Typography level="h4">{genre.name}</Typography>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
