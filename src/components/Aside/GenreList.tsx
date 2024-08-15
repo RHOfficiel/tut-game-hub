1a423b5aa614ce5f0ea7f1482a751eedb8a1fcca
Rehder <trehderk@gmail.com>
2024-08-15 22:35:15 +0200

    Cleaning GenreList component

--- src/components/Aside/GenreList.tsx
+++ src/components/Aside/GenreList.tsx
@@ -23,56 +23,61 @@ const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
   if (isLoading) return <GenreListSkeleton />;
 
   return (
-    <List>
-      {data.map((genre) => (
-        <ListItem key={genre.id} sx={{ paddingY: 1 }}>
-          <ListItemDecorator sx={{ minWidth: 32, minHeight: 32 }}>
-            <AspectRatio
-              ratio="1"
+    <>
+      <Typography paddingX={2} paddingBottom={2} level="h2">
+        Genres
+      </Typography>
+      <List>
+        {data.map((genre) => (
+          <ListItem key={genre.id} sx={{ paddingY: 1 }}>
+            <ListItemDecorator sx={{ minWidth: 32, minHeight: 32 }}>
+              <AspectRatio
+                ratio="1"
+                sx={{
+                  width: 48, // Ensure AspectRatio container is exactly 32x32
+                  height: 48,
+                  borderRadius: "20%", // Optional: to make it circular
+                  overflow: "hidden", // Make sure the image is contained within
+                  marginRight: 2, // Add some space between the image and text
+                }}
+              >
+                <img
+                  src={getCroppedImageURL(genre.image_background)}
+                  srcSet={
+                    getCroppedImageURL(genre.image_background) +
+                    "?auto=format&fit=crop"
+                  }
+                  loading="lazy"
+                  alt={genre.name}
+                  style={{
+                    width: "100%",
+                    height: "100%",
+                    objectFit: "cover", // Ensures the image covers the container
+                  }}
+                />
+              </AspectRatio>
+            </ListItemDecorator>
+
+            <Link
+              overlay
+              underline="none"
+              onClick={() => onSelectGenre(genre)}
               sx={{
-                width: 48, // Ensure AspectRatio container is exactly 32x32
-                height: 48,
-                borderRadius: "20%", // Optional: to make it circular
-                overflow: "hidden", // Make sure the image is contained within
-                marginRight: 2, // Add some space between the image and text
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "start",
               }}
             >
-              <img
-                src={getCroppedImageURL(genre.image_background)}
-                srcSet={
-                  getCroppedImageURL(genre.image_background) +
-                  "?auto=format&fit=crop"
-                }
-                loading="lazy"
-                alt={genre.name}
-                style={{
-                  width: "100%",
-                  height: "100%",
-                  objectFit: "cover", // Ensures the image covers the container
-                }}
-              />
-            </AspectRatio>
-          </ListItemDecorator>
-
-          <Link
-            overlay
-            underline="none"
-            onClick={() => onSelectGenre(genre)}
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "start",
-            }}
-          >
-            <Typography
-              level={genre.id === selectedGenre?.id ? "h4" : "body-lg"}
-            >
-              {genre.name}
-            </Typography>
-          </Link>
-        </ListItem>
-      ))}
-    </List>
+              <Typography
+                level={genre.id === selectedGenre?.id ? "h4" : "body-lg"}
+              >
+                {genre.name}
+              </Typography>
+            </Link>
+          </ListItem>
+        ))}
+      </List>
+    </>
   );
 };
 
