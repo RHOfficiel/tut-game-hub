f5ce5bf61888bb1b83907b7637021338a751cc1f
Rehder <trehderk@gmail.com>
2024-08-15 23:34:46 +0200

    Refractoring GameGrid component

--- src/components/Main/GameGrid.tsx
+++ src/components/Main/GameGrid.tsx
@@ -13,29 +13,29 @@ const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8];
 
+  if (error)
+    return (
+      <Typography color="danger" level="body-md">
+        {error}
+      </Typography>
+    );
+
   return (
-    <>
-      {error && (
-        <Typography color="danger" level="body-md">
-          {error}
-        </Typography>
-      )}
-      <Box
-        component="ul"
-        sx={{
-          display: "flex",
-          gap: 2,
-          flexWrap: "wrap",
-          m: 0,
-        }}
-      >
-        {isLoading &&
-          skeletons.map((skeleton) => <GameCardSkeleton key={skeleton} />)}
-        {data.map((game) => (
-          <GameCard key={game.id} game={game} />
-        ))}
-      </Box>
-    </>
+    <Box
+      component="ul"
+      sx={{
+        display: "flex",
+        gap: 2,
+        flexWrap: "wrap",
+        m: 0,
+      }}
+    >
+      {isLoading &&
+        skeletons.map((skeleton) => <GameCardSkeleton key={skeleton} />)}
+      {data.map((game) => (
+        <GameCard key={game.id} game={game} />
+      ))}
+    </Box>
   );
 };
 
